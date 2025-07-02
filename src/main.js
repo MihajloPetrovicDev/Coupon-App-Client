33dcb61df1552f37f37481afd5ad689d8bf98b0e
MihajloPetrovicDev <petrovicmihajlo789@gmail.com>
2025-07-02 23:24:39 +0200

    Add Lunasima font, Tailwind CSS and SASS
    - Add postcss, autoprefixer, sass, sass-loader, tailwindcss and @tailwindcss/postcss to devDependencies in package.json
    - Register Tailwind CSS as a PostCSS plugin in vue.config.js
    - Remove style section from App.vue
    - Add main.scss and _variables.scss
    - Import main.scss in main.js
    - Add Lunasima Regular and Lunasima Bold fonts to the assets folder

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import config from './config';
+import './styles/main.scss'
 
 const app = createApp(App);
 
